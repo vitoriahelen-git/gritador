c35c879179e596b2ccdbb747984fdd44fa7d75fd
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-11 23:57:13 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alta; 
+            Console.Write("Escreva uma mensagem: ");
+            alta = Console.ReadLine().ToUpper(); 
+            Console.WriteLine();
+            Console.WriteLine(alta); 
+
+        }
+    }
+}
